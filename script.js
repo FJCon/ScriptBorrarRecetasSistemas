e3cf16662d876ca491ed480a28fc320881eedef1
Fernando Conil <107715084+FJCon@users.noreply.github.com>
2023-08-08 08:56:50 -0300

    Busqueda con texto en minúsculas

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let tabla = document.querySelector('body').children[1].children[0].children; //array con todas las filas de la tabla
 let busqueda = prompt('Ingrese equipo a eliminar');
+busqueda = busqueda.toUpperCase();
 for (let i = 3; i < tabla.length; i++){
     if(tabla[i].children[2].children[0].children[0].innerText == busqueda){
         let link = 'http://192.168.0.2/Obras/';
